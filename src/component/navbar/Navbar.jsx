8d8e564a96ed93db6a9307dc656af6d5d5aa130c
horpemee <arifoopeyemi@gmail.com>
2023-03-13 03:31:28 +0100

    removed client id and client secret

--- src/component/navbar/Navbar.jsx
+++ src/component/navbar/Navbar.jsx
@@ -1,80 +1,68 @@
-import { useState, useEffect } from "react";
+// import { useState, useEffect } from "react";
 import "./navbar.scss";
 // import axios from "axios";
 import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
 
-const CLIENT_ID = "f7f0a18112ba4a049ff76982cb60524f";
-const CLIENT_SECRET = "c39ef23cd3a44325a91797b5779bd1f1";
-
 const Navbar = () => {
-  const [searchInput, setSearchInput] = useState("");
-  const [accessToken, setAccessToken] = useState("");
+  // const [searchInput, setSearchInput] = useState("");
+  // const [accessToken, setAccessToken] = useState("");
   // const [artists, setArtists] = useState([]);
 
-  useEffect(() => {
-    //API access Token
-    var authParameters = {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/x-www-form-urlencoded",
-      },
-      body:
-        "grant_type=client_credentials&client_id=" +
-        CLIENT_ID +
-        "&client_secret=" +
-        CLIENT_SECRET,
-    };
-
-    fetch("https://accounts.spotify.com/api/token", authParameters)
-      .then((result) => result.json())
-      .then((data) => setAccessToken(data.access_token));
-  }, []);
-
-  //Search
-  async function search() {
-    console.log("Search for " + searchInput);
-
-    //Get request using search to get the Artist ID
-    var artistParameters = {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: "Bearer " + accessToken,
-      },
-    };
-
-    var artistID = await fetch(
-      "https://api.spotify.com/v1/search?q=" + searchInput + "&type=artist",
-      artistParameters
-    )
-      .then((response) => response.json())
-      .then((data) => console.log(data));
-  }
-
-  // const searchArtists = async (e) => {
-  //   e.preventDefault();
-  //   const { data } = await axios.get("https://api.spotify.com/v1/search", {
+  // useEffect(() => {
+  //   //API access Token
+  //   var authParameters = {
+  //     method: "POST",
   //     headers: {
-  //       Authorization: `Bearer `,
+  //       "Content-Type": "application/x-www-form-urlencoded",
   //     },
-  //     params: {
-  //       q: "searchKey",
-  //       type: "artist",
+  //     body:
+  //       "grant_type=client_credentials&client_id=" +
+  //       CLIENT_ID +
+  //       "&client_secret=" +
+  //       CLIENT_SECRET,
+  //   };
+
+  //   fetch("https://accounts.spotify.com/api/token", authParameters)
+  //     .then((result) => result.json())
+  //     .then((data) => setAccessToken(data.access_token));
+  // }, []);
+
+  // //Search
+  // async function search() {
+  //   console.log("Search for " + searchInput);
+
+  //   // Get request using search to get the Artist ID
+  //   var searchParameters = {
+  //     method: "GET",
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //       Authorization: "Bearer " + accessToken,
   //     },
-  //   });
+  //   };
 
-  //   setArtists(data.artists.items);
-  // };
+  //   var artistID = await fetch(
+  //     "https://api.spotify.com/v1/search?q=" + searchInput + "&type=artist",
+  //     searchParameters
+  //   )
+  //     .then((response) => response.json())
+  //     .then((data) => {
+  //       return data.artists.items[0].id;
+  //     });
+  //   console.log("Artist ID is " + artistID);
 
-  // const handleInputChange = (event) => {
-  //   setQuery(event.target.value);
-  // };
+  //   var albums = await fetch(
+  //     "https://api.spotify.com/v1/artists/" +
+  //       artistID +
+  //       "/albums" +
+  //       "?include_groups=album&market=US&limit=50",
+  //     searchParameters
+  //   )
+  //     .then((response) => response.json())
+  //     .then((data) => {
+  //       console.log(data);
+  //     });
+  // }
 
-  // const handleSearch = async (event) => {
-  //   event.preventDefault();
-  // };
-
-  // const response = await fetch('https:api.spotify.com/v1/search?q=$}{query}&type=artist')
   return (
     <div className="navbar">
       <form>
@@ -83,17 +71,20 @@ const Navbar = () => {
           <input
             type="text"
             placeholder="Search artist"
-            onKeyPress={(event) => {
-              if (event.key === "Enter") {
-                search();
-              }
-            }}
-            onChange={(event) => setSearchInput(event.target.value)}
-            className="search"
+            // onKeyPress={(event) => {
+            //   if (event.key === "Enter") {
+            //     search();
+            //   }
+            // }}
+            // onChange={(event) => setSearchInput(event.target.value)}
+            // className="search"
           />
         </div>
 
-        <a href="http://localhost:8888" className="login">
+        <a
+          href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}
+          className="login"
+        >
           Login to Spotify
         </a>
       </form>
